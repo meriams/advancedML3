90f2adb3bb8c4920ba373573162990141e238b58
chphardi <94768337+chphardi@users.noreply.github.com>
2024-04-25 12:10:58 +0200

    Add files via upload

---
+++ Weisfeiler-Lehman_test.py
@@ -0,0 +1,26 @@
+import networkx as nx
+from torch_geometric.datasets import TUDataset
+from torch_geometric.loader import DataLoader
+
+# Load the MUTAG dataset
+mutag = TUDataset(root='./data/', name='MUTAG')
+
+train_loader = DataLoader(mutag, batch_size=1)
+
+wl_list = []
+
+for data in train_loader:
+    G = nx.Graph()
+
+    for index, features in enumerate(data.x):
+        G.add_node(index, features=tuple(features.tolist()))
+
+    for i in range(data.edge_index.shape[1]):
+        source = data.edge_index[0, i].item()  # source node index
+        target = data.edge_index[1, i].item()  # target node index
+        G.add_edge(source, target)
+
+    wl_list.append(nx.weisfeiler_lehman_graph_hash(G))
+
+
+
