18cd10f09da398c9bffe4f4d7dcc3dae4c7273dd
Thor <thor1458@gmail.com>
2024-04-28 11:40:36 +0200

    Update

--- sample_data.py
+++ sample_data.py
@@ -1,24 +1,33 @@
 import networkx as nx 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 from torch_geometric.datasets import TUDataset
 from torch_geometric.loader import DataLoader
+import random
+import numpy as np
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
 def get_training_values():
     mutag = TUDataset(root='./data/', name='MUTAG')
 
     train_loader = DataLoader(mutag, batch_size=1)
-    total_possible_edges = 0
-    total_edges = 0
+    node_list = []
+    probability_list = []
     max_nodes = 0
     for data in train_loader:
         max_nodes = max(max_nodes, data.num_nodes)
 
-    for data in train_loader:
-        for e in range(max_nodes):
-            total_possible_edges += e
-        total_edges += len(data.edge_index[0])
 
-    return total_possible_edges, total_edges
+    for data in train_loader:
+        total_possible_edges = 0
+        total_edges = 0
+        for num in range(data.num_nodes):
+            total_possible_edges += num
+        probability = len(data.edge_index[0]) / total_possible_edges
+        node_list.append(data.num_nodes)
+        probability_list.append(probability)
+
+    return node_list, probability_list, max_nodes
 
 
 def get_training_graph():
@@ -86,8 +95,31 @@ def graph_statistics(Graphs):
         eigenvector_centrality.append(nx.eigenvector_centrality(G, max_iter=10000))
     return node_degree, clustering_coefficient, eigenvector_centrality
 
-def show_graphs(degree_graph, clustering_graph, eigenvector_graph):
-    data = [degree_graph, clustering_graph, eigenvector_graph]
+def get_buckets(node_degree, clustering_coefficient, eigenvector_centrality):
+    degree_bucket = []
+    clustering_bucket = []
+    eigenvector_bucket = []
+    for index in range(1000):
+        for val in node_degree[index]:
+            degree_bucket.append(val[1])
+        for val in clustering_coefficient[index].items():
+            clustering_bucket.append(val[1])
+        for val in eigenvector_centrality[index].items():
+            eigenvector_bucket.append(val[1])
+    return degree_bucket, clustering_bucket, eigenvector_bucket
+
+def get_min_max(val_list):
+    max_val = 0
+    min_val = int(1e9)
+    for val in val_list:
+        max_val = max(max_val, val)
+        min_val = min(min_val, val)
+    return max_val, min_val
+
+
+def show_graphs(degree_list, clustering_list, eigenvector_list):
+    degree_bucket, clustering_bucket, eigenvector_bucket = get_buckets(degree_list, clustering_list, eigenvector_list)
+    data = [degree_bucket, clustering_bucket, eigenvector_bucket]
     titles = ['Node Degree', 'Clustering Coefficient', 'Eigenvector Centrality']
     for i, d in enumerate(data):
         plt.figure()
@@ -96,17 +128,31 @@ def show_graphs(degree_graph, clustering_graph, eigenvector_graph):
         plt.xlabel('Interval')
         plt.xlabel('Amount')
         plt.show()
-def main():
-    baseline_graph = [nx.erdos_renyi_graph(28, p=(7442 / 71064)) for _ in range(1000)]
 
-    vae_graph = []
+def main():
+    training_node_list, training_probability_list, max_nodes = get_training_values()
+    max_index = len(training_node_list) - 1
+    baseline_graphs = []
     for i in range(1000):
-        graph = nx.read_adjlist(f"graphs/graph_{i}.adjlist")
-        vae_graph.append(graph)
-    
+        ran_index = random.randint(0,max_index)
+        temp_G = nx.erdos_renyi_graph(training_node_list[ran_index], p=training_probability_list[ran_index])
+        temp_G.remove_nodes_from(list(nx.isolates(temp_G)))
+        baseline_graphs.append(temp_G)
+
+    vae_graphs = []
+    for i in range(1000):
+        graph = nx.read_adjlist(f"graphs/graphs/graph_{i}.adjlist")
+        vae_graphs.append(graph)
+
     training_graph = get_training_graph()
     training_hashes = get_hashes_from_graphs(training_graph)
-    evaluation = sample_evaluation(baseline_graph, training_hashes)
-    novelty_results = full_evaluation(baseline_graph, vae_graph, training_hashes)
-    node_degree, clustering_coefficient, eigenvector_centrality = graph_statistics(baseline_graph)
-    # show_graphs(node_degree, clustering_coefficient, eigenvector_centrality)
+    # evaluation = sample_evaluation(baseline_graphs, training_hashes)
+    novelty_results = full_evaluation(baseline_graphs, vae_graphs, training_hashes)
+    print(novelty_results)
+    base_node_degree, base_clustering_coefficient, base_eigenvector_centrality = graph_statistics(baseline_graphs)
+    vae_node_degree, vae_clustering_coefficient, vae_eigenvector_centrality = graph_statistics(vae_graphs)
+    show_graphs(base_node_degree, base_clustering_coefficient, base_eigenvector_centrality)
+    show_graphs(vae_node_degree, vae_clustering_coefficient, vae_eigenvector_centrality)
+
+
+main()
